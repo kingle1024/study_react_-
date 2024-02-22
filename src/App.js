c9691d1ab4455562d52e58d8163a16944407c147
엄지용 <teran1024@naver.com>
2024-02-22 15:57:56 +0900

    React에서 API 호출하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import LifeCycle from './LifeCycle';
-import LifeCycle2 from "./LifeCycle2";
 
 const App = () => {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments'
+    ).then((res) => res.json());
+    console.log(res);
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5)+1,
+        created_date: new Date().getTime(),
+        id: dataId.current++
+      }
+    })
+
+    setData(initData);
+  };
+
+  useEffect(() => { // mount 되는 시점에 바로 실행됨
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -42,7 +62,6 @@ const App = () => {
   return (
     <div className="App">
       <h2>일기장</h2>
-      <LifeCycle2 />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onRemove={onRemove} onEdit={onEdit} diaryList={data} />
     </div>
