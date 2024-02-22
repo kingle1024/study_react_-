cfe5437c2e0a628eb70e5c94c119f69767adf630
엄지용 <teran1024@naver.com>
2024-02-22 17:23:15 +0900

    React.memo 최적화 처리. 객체의 경우 얕은 비교 방지 처리

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,34 +1,50 @@
 import React, {useState, useEffect} from 'react';
 
-const TextView = React.memo(({text}) => {
-useEffect(() => {
-  console.log(`Update :: Text : ${text}`);
-})
+const CounterA = React.memo(({count}) => {
 
-  return <div>{text}</div>;
-});
-const CountView = React.memo(({count}) => {
   useEffect(() => {
-    console.log(`Update :: Count : ${count}`);
-  });
-  
+    console.log(`CounterA Update - count: ${count}`);
+  })
+
   return <div>{count}</div>
 });
+const CounterB = ({obj}) => {
+
+  useEffect(() => { // 객체가 주소에 의한 비교인 얕은 비교를 하기 때문에 실행된다. 
+    console.log(`CounterB Update - count : ${obj.count}`);
+  })
+  return <div>{obj.count}</div>
+};
+
+const areEqual = (prevProps, nextProps) => {
+  return prevProps.obj.count === nextProps.obj.count;
+}
+const MemorizedCounterB = React.memo(CounterB, areEqual);
 
 const OptiomizeTest = () => {
   const [count, setCount] = useState(1);
-  const [text, setText] = useState('');
+  const [obj, setObj] = useState({
+    count: 1
+  })
 
   return <div style={{ padding: 50}}>
     <div>
-      <h2>count</h2>
-      <CountView count={count} />
-      <button onClick={() => setCount(count+1)}>+</button>
+      <h2>Counter A</h2>
+      <CounterA count={count} />
+      <button onClick={() => setCount(count)}>A button</button>
     </div>
     <div>
-      <h2>text</h2>
-      <TextView text={text} />
-      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <h2>Counter B</h2>
+      <MemorizedCounterB obj={obj} />
+      <button
+        onClick={() =>
+          setObj({
+            count: obj.count,
+          })
+        }
+      >
+      B button  
+      </button>
     </div>
   </div>  
 }
